d1e21d2d03d74745830f0a5e6917c56bd5b0df7f
WEI <95195127+GNK48-WZY@users.noreply.github.com>
2025-02-14 20:53:03 -0500

    Update main.py

--- dedalus/main.py
+++ dedalus/main.py
@@ -26,6 +26,7 @@ epsilon = 4e-5
 Lx, Lz = 2., 1.
 Nx, Nz = 384, 192
 stop_sim_time = 100 + 300*restart # Stopping criteria
+timestepper = d3.RK222
 ############ parameter settings
 
 
@@ -42,26 +43,48 @@ p = dist.Field(name='p', bases=(xbasis, zbasis))
 Cxx = dist.Field(name='Cxx', bases=(xbasis, zbasis))
 Cxz = dist.Field(name='Cxy', bases=(xbasis, zbasis))
 Czz = dist.Field(name='Cyy', bases=(xbasis, zbasis))
+tau_p = dist.Field(name='tau_p')
+tau_u1 = dist.VectorField(coords, name='tau_u1', bases=xbasis)
+tau_b1 = dist.Field(name='tau_b1', bases=xbasis)
 
 # Substitutions
 x, z = dist.local_grids(xbasis, zbasis)
 ex, ez = coords.unit_vector_fields(dist)
+lift = lambda A: d3.Lift(A, zbasis.derivative_basis(1), -1)
+grad_u = d3.grad(u)
+ux = u@ex
+uz = u@ez
 
-# FENE-P Model Terms
+# Velocity gradient components
+dx_ux = grad_u['x','x']
+dz_ux = grad_u['x','z']
+dx_uz = grad_u['z','x']
+dz_uz = grad_u['z','z']
+
+# FENE-P model terms (corrected trC adjustment for 2D)
 trC = Cxx + Czz
-f = 1 / ((1 - (trC - 3))/Lmax**2) 
+f = 1 / (1 - (trC - 2)/Lmax**2)  # 2D adjustment (equilibrium trC=2)
 Txx = (Cxx * f - 1) / Wi
 Txz = (Cxz * f) / Wi
 Tzz = (Czz * f - 1) / Wi
 
-# Problem Setup
-problem = d3.IVP([u, p, Cxx, Cxz, Czz], namespace=locals())
+# Problem setup with corrected variables and equations
+problem = d3.IVP([u, p, Cxx, Cxz, Czz, tau_p], namespace=locals())
 
-# Momentum equation (linear LHS)
+# Momentum equation
 problem.add_equation(
     "Re*dt(u) + Re*grad(p) - beta*div(grad(u)) = "
     "Re*dot(u, grad(u)) + (1-beta)*div(Txx*ex*ex + Txz*ex*ez + Txz*ez*ex + Tzz*ez*ez)"
 )
 
-# Incompressibility condition
-problem.add_equation("div(u) = 0")
+# Incompressibility and pressure gauge
+problem.add_equation("div(u) + tau_p = 0")
+
+# Constitutive equations for FENE-P model
+problem.add_equation("dt(Cxx) + dot(u, grad(Cxx)) = 2*Cxx*dx_ux + 2*Cxz*dz_ux - (f*(Cxx - 1))/Wi")
+problem.add_equation("dt(Cxz) + dot(u, grad(Cxz)) = Cxz*(dx_ux + dz_uz) + Cxx*dx_uz + Czz*dz_ux - (f*Cxz)/Wi")
+problem.add_equation("dt(Czz) + dot(u, grad(Czz)) = 2*Cxz*dx_uz + 2*Czz*dz_uz - (f*(Czz - 1))/Wi")
+
+# Solver
+solver = problem.build_solver(timestepper)
+solver.stop_sim_time = stop_sim_time
